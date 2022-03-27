fcb6b52aac987391e8fe99096d3ccc971ce85b25
Nguyen Thi Hong Nhung <hongnhung22022000@gmail.com>
2022-03-27 16:06:10 +0700

    taibai

---
+++ ss02_java/ss02_java/src/service/Impl/FacilityServiceImpl.java
@@ -0,0 +1,35 @@
+package service.Impl;
+
+import models.Facility;
+import service.FacilityService;
+
+import java.util.LinkedHashMap;
+import java.util.Map;
+import java.util.Scanner;
+import java.util.Stack;
+
+public class FacilityServiceImpl implements FacilityService {
+    private static Map<Facility, Integer>facilityIntegerMap = new LinkedHashMap<>();
+    private static Scanner scanner = new Scanner(System.in);
+
+    @Override
+    public void display() {
+
+    }
+
+    @Override
+    public void displayMaintain() {
+
+    }
+
+    @Override
+    public void addNewVilla() {
+
+
+    }
+
+    @Override
+    public void addNewRoom() {
+
+    }
+}
